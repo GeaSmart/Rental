23dffbe0970d292c87eacbe6b6748de80cd627b9
Gerson Eder Azabache Martinez <GAzabache@CheckAlt-Eras.com>
2021-07-30 11:16:02 -0500

    ¿first controller created

---
+++ Mach/Controllers/ContratoController.cs
@@ -0,0 +1,48 @@
+﻿using Mach.Entities;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Mach.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class ContratoController:ControllerBase
+    {
+        public ContratoController()
+        {
+
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<List<Contrato>>> Get()
+        {
+            return new List<Contrato>() {
+                new Contrato(){ 
+                    FechaInicio = new DateTime(),
+                    FechaFin = new DateTime(),
+                    IsCombustible=false, 
+                    IsTransporte=true, 
+                    Observaciones="ninguna obs"
+                },
+                new Contrato(){
+                    FechaInicio = new DateTime(),
+                    FechaFin = new DateTime(),
+                    IsCombustible=false,
+                    IsTransporte=true,
+                    Observaciones="alguna obs"
+                },
+                new Contrato(){
+                    FechaInicio = new DateTime(),
+                    FechaFin = new DateTime(),
+                    IsCombustible=false,
+                    IsTransporte=true,
+                    Observaciones="finalmente obs"
+                },
+            };
+        }
+
+    }
+}
